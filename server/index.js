d29d23b538fe5b30831700db8ae22d3cd11795af
Anna Maria Theresa Hermoso <amt.hermoso@gmail.com>
2024-11-21 13:02:07 +0800

    temporary landing page
    temporary landing page, more changes to come

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the current directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Serve static files like CSS from the 'css' folder
+app.use('/css', express.static(path.join(__dirname, '..', 'css')));
+
+// Serve the 'index.html' from the 'html' folder
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, '..', 'html', 'index.html'));
 });
 
 app.listen(3000, () => {
